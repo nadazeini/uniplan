0798a7aae3ebd21ded3a0fd0822c7d95c18ca3f4
Nada Zeini <nada.elzeini@sjsu.edu>
2021-03-03 12:03:49 -0800

    Setup server

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+require("./models/teacher");
+
+app.use(express.json());
+
+app.use(require("./routes/home"));
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting to db: ", err);
+});
+app.listen(PORT, () => {
+  console.log("Server is running on ", PORT);
+});
