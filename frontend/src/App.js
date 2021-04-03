daa2edd991c5d2f436dcc8cde433c4e1a234606f
Aakash Nayyar <anayyar02@gmail.com>
2021-04-02 21:36:43 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,7 +41,7 @@ export default function App() {
       setSearched(true);
   };
 
-  function avg(arr){
+  function avg(arr=[]){
     var sum = 0;
     for (var i = 0; i < arr.length; i++) {
       sum+=arr[i];
