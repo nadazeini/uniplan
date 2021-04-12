4e6909ccf00956d33b65a247aeac2569347afef7
Diljot Singh <diljot.singh@sjsu.edu>
2021-04-11 22:09:32 -0700

    restructured courseplan schema

--- backend/models/courseplan.js
+++ backend/models/courseplan.js
@@ -1,26 +1,11 @@
 const mongoose = require("mongoose");
 
 const coursePlanSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-
-  department:
-  {
-    type: String,
-    required: true,
-  },
-  number:
-  {
-    type: Number,
-    required: true,
-  },
-  description: String,
-  teachers: [String],
-  semestersOffered: [{semester:String, year:Number}],
-  reviewsGiven: [{teacher: String, review: String}],
-  ratingsGiven:[{teacher: String, review: String}]
+[{
+  year: Number,
+  term: String,
+  coursesTaken: [{course:String, teacher:String}],
+}]
 });
 
 mongoose.model("CoursePlan", coursePlanSchema);
