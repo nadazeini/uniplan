104e69b38b96a15d4797370186fbd0d5cdf6503e
Diljot Singh <diljot.singh@sjsu.edu>
2021-04-12 01:48:54 -0700

    added add-class route

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -34,12 +34,39 @@ router.post("/add-teacher", (req, res) => {
 router.post("/search-teachers", (req, res) => {
   const { name } = req.body;
   Teacher.find({ name: name })
-    .then((teacher) => { 
+    .then((teacher) => {
       //response is teacher obj
       res.json({ teacher });
     })
     .catch((err) => {
       res.json({ error: err });
     });
+});
+
+
+router.post("/add-class", (req, res) => {
+  const {course, teacher} = req.body;
+  if (!name) {
+    return res.status(422).json({ error: "need course name" });
+  }
+  Student.findOne({ course: course, teacher: teacher })
+    .then((existingCourse) => {
+      if (existingCourse) {
+        return res.status(422).json({ error: "this course already exists" });
+      }
+      const course = new Course({
+        course,
+        teacher,
+      });
+      course
+        .save()
+        .then((course) => {
+          res.json({ message: "course added" });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => console.log(err));
 });
 module.exports = router;
