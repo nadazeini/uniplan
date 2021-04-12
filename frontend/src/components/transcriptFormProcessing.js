ce08048dab7844c743e9b9aa99d1dd3b60b50781
Nada Zeini <nada.elzeini@sjsu.edu>
2021-04-12 16:11:17 -0700

    Changed transcript component

--- frontend/src/components/transcriptFormProcessing.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default function TranscriptFormProcessing() {
-    return(
-        <div>
-        Transcriot Form Processing  
-        </div>
-    );
-}
