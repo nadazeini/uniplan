023478fbf7f2871dfd8338caad6b6b0abbaf2d78
Nada Zeini <nada.elzeini@sjsu.edu>
2021-04-22 23:56:44 -0700

    Adjust label

--- frontend/src/components/TermAndYear.js
+++ frontend/src/components/TermAndYear.js
@@ -5,10 +5,7 @@ export const TermAndYear = ({ semester, completeSemester }) => {
     <div
       style={{
         display: "inline-block",
-        border: "2px solid black",
         borderRadius: "10px",
-        padding: "10px 20px",
-        margin: "10px",
       }}
       key={semester.id}
       onClick={() => completeSemester(semester.id)}
