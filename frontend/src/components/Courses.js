5857d5873a62dbff6cb3444ea50e5a2cda9b057d
Nada Zeini <nada.elzeini@sjsu.edu>
2021-04-24 23:36:28 -0700

    Delete/update Course logic change

--- frontend/src/components/Courses.js
+++ frontend/src/components/Courses.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Course from "./Course";
 
-const Courses = ({ courses, removeCourse, updateCourse }) => {
+const Courses = ({ semester }) => {
+  //using state to handle remove/delete course
+  //adding is just modifying props
+  const [courses, setCourses] = useState([]); //using this to rerender comp after delete/update
+
+  //will re-render all courses everytime semester.courses changes
+  useEffect(() => {
+    // console.log("adding, removing ");
+    // console.log(courses);
+  }, [courses]); //or semester.courses
+
+  const updateCourse = (courseId, newValue) => {
+    if (!newValue.name || /^\s*$/.test(newValue.name)) {
+      return;
+    }
+    setCourses((prev) =>
+      prev.map((item) => (item.id === courseId ? newValue : item))
+    );
+    //find course to update in courses of the semester
+    courses.forEach((course) => {
+      if (course.id === courseId) {
+        course.name = newValue.name;
+      }
+    });
+    // console.log(semester.courses);
+  };
+
+  const removeCourse = (id) => {
+    const removedArr = [...semester.courses].filter(
+      (course) => course.id !== id
+    );
+    semester.courses = removedArr.slice(0);
+    setCourses(removedArr);
+  };
+
   return (
     <div>
-      {courses.map((course, index) => {
+      {semester.courses.map((course, index) => {
         return (
           <Course
             key={index}
