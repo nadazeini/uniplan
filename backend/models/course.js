9e82fe4e0fd8035cf8e8ffb4b7a504c0d1b25339
Nada Zeini <nada.elzeini@sjsu.edu>
2021-04-26 22:36:57 -0700

    No errors when id required

--- backend/models/course.js
+++ backend/models/course.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const courseSchema = new mongoose.Schema({
   id: {
     type: Number,
-    // required: true,
+    required: true,
   },
   name: {
     type: String,
