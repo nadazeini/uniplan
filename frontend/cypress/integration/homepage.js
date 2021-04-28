5dbbd0c208c9ab27bd6dd63f7385f297c6602346
Nada Zeini <nada.elzeini@sjsu.edu>
2021-04-27 23:24:04 -0700

    Check if navbar renders

--- frontend/cypress/integration/homepage.js
+++ frontend/cypress/integration/homepage.js
@@ -2,5 +2,6 @@ describe("renders the home page", () => {
   it("renders correctly", () => {
     cy.visit("/");
     cy.get("#container").should("exist");
+    cy.get("#navbar").should("exist");
   });
 });
