bbe62d007749bfce230822c794ef2dc1cb5e1038
Nada Zeini <nada.elzeini@sjsu.edu>
2021-04-28 17:35:29 -0700

    Formatting + added div id for testing

--- frontend/src/pages/loginpage.js
+++ frontend/src/pages/loginpage.js
@@ -4,84 +4,81 @@ import React, { useState } from "react";
 import { BrowserRouter as Switch, Link } from "react-router-dom";
 
 export default function LoginPage() {
-
-  const[loggedIn,setloggedIn]= useState(false);
+  const [loggedIn, setloggedIn] = useState(false);
   const [results, setResults] = useState({});
 
-  const checkCredentials = (email,password) => {
+  const checkCredentials = (email, password) => {
     const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({email: email, password: password}),
+      body: JSON.stringify({ email: email, password: password }),
     };
 
     fetch("http://localhost:5000/log-in/", requestOptions)
       .then((res) => res.json())
-      
+
       .then((res) => setResults(res.user));
 
     setloggedIn(true);
   };
 
-
-  if(loggedIn){
-    return(
-      document.location.href = "/"
-      );
-  }
-  else{
-      return(
-        <div className="App">
+  if (loggedIn) {
+    return (document.location.href = "/");
+  } else {
+    return (
+      <div className="App" id="login-page">
         <div className="">
           <img alt="" src={logo6} width="10%" />
-          <div className="title">
-            LOGIN
-          </div>
+          <div className="title">LOGIN</div>
         </div>
 
         <div className="wrap">
           <h3>Log Into Your Account</h3>
-
-          <input type = "email"
-            placeholder="Email"
-            id = "email"
-            required
-          />
+          <input type="email" placeholder="Email" id="email" required />
           <br></br> <br></br>
-          <input type = "password"
+          <input
+            type="password"
             placeholder="Password"
-            id = "password"
+            id="password"
             required
           />
           <br></br> <br></br>
-          <button type = "submit"
-          onClick = {() => {
-            if(document.getElementById("email").value === "" || document.getElementById("password").value === ""){
-              alert("Error: All fields must be filled out!")
-            }
-            else if(document.getElementById("email").value.substring(document.getElementById("email").value.length-4)!==".edu"){
-              alert("Error: You must use a school email.")
-            }
-            else{
-              checkCredentials(document.getElementById("email").value,document.getElementById("password").value,);
-            }
-          }}
+          <button
+            id="login-button"
+            type="submit"
+            onClick={() => {
+              if (
+                document.getElementById("email").value === "" ||
+                document.getElementById("password").value === ""
+              ) {
+                alert("Error: All fields must be filled out!");
+              } else if (
+                document
+                  .getElementById("email")
+                  .value.substring(
+                    document.getElementById("email").value.length - 4
+                  ) !== ".edu"
+              ) {
+                alert("Error: You must use a school email.");
+              } else {
+                checkCredentials(
+                  document.getElementById("email").value,
+                  document.getElementById("password").value
+                );
+              }
+            }}
           >
-          LOGIN
+            LOGIN
           </button>
-
           <br></br>
           <div>
-          Need an account?
-          <a class="" href="#">
+            Need an account?
+            <a class="" href="#">
               <Link to="/signup"> Sign Up</Link>
             </a>
           </div>
-
-
         </div>
-
       </div>
-      );
+    );
   }
 }
